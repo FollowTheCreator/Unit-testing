f6d0ca2a28e4214902c7cfdfea796355a9567339
Artyom <Artyomkovalevich.top@gmail.com>
2019-09-04 16:44:01 +0300

    Create round-robin collection

---
+++ RoundRobinCollection/RoundRobinCollection.Tests/RoundRobinCollection/RoundRobinCollectionTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoundRobinCollection.Tests.RoundRobinCollection
+{
+    public class RoundRobinCollectionTests
+    {
+
+    }
+}
---
+++ RoundRobinCollection/RoundRobinCollection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RoundRobinCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new RoundRobinCollection<int>(1, 2, 3, 4);
+            var firstInstance = collection.GetEnumerator();
+            var secondInstance = collection.GetEnumerator();
+            var thirdInstance = collection.GetEnumerator();
+
+            Console.WriteLine(firstInstance.Current);
+            firstInstance.MoveNext();
+            Console.WriteLine(secondInstance.Current);
+            secondInstance.MoveNext();
+            Console.WriteLine(firstInstance.Current);
+            firstInstance.MoveNext();
+
+            var collection2 = new RoundRobinCollection<int>(5, 6, 7, 8);
+
+            var firstInstance2 = collection2.GetEnumerator();
+            var secondInstance2 = collection2.GetEnumerator();
+
+            Console.WriteLine(firstInstance2.Current);
+            firstInstance2.MoveNext();
+            Console.WriteLine(secondInstance2.Current);
+            secondInstance2.MoveNext();
+            Console.WriteLine(firstInstance2.Current);
+            firstInstance2.MoveNext();
+        }
+    }
+}
